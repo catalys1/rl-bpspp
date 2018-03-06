876a3bf0c41f4f5f709de3f451768d9f0f09b95a
William Myers <mwilliammyers@gmail.com>
2018-03-06 12:49:19 -0700

    Update main.py
    - Work in progress
    - Add optional bias setting

--- main.py
+++ main.py
@@ -6,34 +6,39 @@ import scipy.stats
 
 
 class Model:
-    def __init__(self, env, render_mode=None):
+    def __init__(self, env, bias=None, render_mode=None):
         self.env = env
 
-        # TODO: do these need to change throughout the program?
-        self._bias = scipy.stats.dirichlet([1.0] * env.action_space.n)
-        self.bias = self._bias.rvs()[0]
+        if bias is None:
+            self._bias = scipy.stats.dirichlet([1.0] * env.action_space.n)
+            self.bias = self._bias.rvs()[0]
+        else:
+            self._bias = None
+            self.bias = bias
+
         self._policy = scipy.stats.multinomial(n=1, p=self.bias)
         self.policy = self._policy.rvs(size=env.observation_space.shape)
 
         self.render_mode = render_mode
         self.reward = self.run(render_mode=self.render_mode)
 
-    def propose_policy(self, from_policy=None):
+    def propose_policy(self, from_policy=None, num_states_to_change=1):
         if from_policy is None:
             from_policy = self.policy
 
-        if np.random.randint(0, 2) == 0:
+        if self._bias is not None and np.random.randint(0, 2) == 0:
             # TODO: this doesnt seem right? just sample it again?
             self._bias = scipy.stats.dirichlet([1.0] * self.env.action_space.n)
             self.bias = self._bias.rvs()[0]
         else:
             from_policy = np.copy(from_policy)
-            state_row = np.random.randint(0, from_policy.shape[0])
-            state_col = np.random.randint(0, from_policy.shape[1])
-            action = np.random.randint(0, self.env.action_space.n)
-            distribution = np.zeros(self.env.action_space.n)
-            distribution[action] = 1.0
-            from_policy[state_row, state_col] = distribution
+            for i in range(num_states_to_change):
+                state_row = np.random.randint(0, from_policy.shape[0])
+                state_col = np.random.randint(0, from_policy.shape[1])
+                action = np.random.randint(0, self.env.action_space.n)
+                distribution = np.zeros(self.env.action_space.n)
+                distribution[action] = 1.0
+                from_policy[state_row, state_col] = distribution
 
         return from_policy
 
@@ -67,7 +72,7 @@ class Model:
         if reward is None:
             reward = self.reward
 
-        ll = self._bias.pdf(self.bias)
+        ll = 0.0 if self._bias is None else self._bias.pdf(self.bias)
         # TODO: do we have to loop?
         for p in policy:
             for x in p:
@@ -93,7 +98,7 @@ def metropolis_hastings(model, iterations=1000):
 def main(search_for_policy=metropolis_hastings):
     env = gym.make("openmaze-v0")  # TODO: change to random
 
-    model = Model(env, render_mode='human')
+    model = Model(env, bias=[.85, .05, .05, .05], render_mode='human')
     start_policy = model.policy
     model = search_for_policy(model)
     print(np.abs(model.policy - start_policy).sum())
