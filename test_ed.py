5ada5f63f18b821a4a2803e603daa94d77e0dd1b
William Myers <mwilliammyers@gmail.com>
2018-03-09 00:06:22 -0700

    Update test_ed.py
    - WIP

--- test_ed.py
+++ test_ed.py
@@ -1,84 +1,91 @@
-import itertools
 import collections
+import itertools
+import pprint
 
 import edward as ed
 import gym
+import gym_openmaze
 import numpy as np
 import tensorflow as tf
 import tqdm
 
-# tf.contrib.eager.enable_eager_execution()
+
+def _normalize(v, ord=1, axis=-1):
+    norm = np.linalg.norm(v, ord=ord, axis=axis)
+    if norm == 0:
+        norm = np.finfo(v.dtype).eps
+    return v / norm
 
 
-def run_maze(env, policy, render_mode=None, render_step=1, max_previous_states=2):
-    total_reward = 0.0
-    state = tuple(env.reset())
-    # path = {state}
+def run_maze(env, policy, render_mode=None, render_step=1, max_previous_states=4, max_duplicate_states=None):
+    if max_duplicate_states is None:
+        max_duplicate_states = max(1, round(max_previous_states / 3))
+
     previous_states = collections.deque(maxlen=max_previous_states)
-    for i in itertools.count():
+    state = tuple(env.reset())
+    total_reward = 0.0
+    for step_count in itertools.count():
         previous_states.append(state)
         action = np.random.choice(env.action_space.n, p=policy[state])
-        observation, reward, done, info = env.step(action)
+        observation, reward, done, _ = env.step(action)
 
-        if render_mode is not None and i % render_step == 0:
+        if render_mode is not None and step_count % render_step == 0:
             env.render(mode=render_mode)
 
         total_reward += reward
 
         obs = tuple(observation)
-        # print(state, obs, state == obs)
-        if (len(previous_states) >= max_previous_states and obs in previous_states) or done:
+        if (len(previous_states) >= max_previous_states and previous_states.count(obs) >= max_duplicate_states) or done:
             break
         state = obs
-        # path.add(state)
-    return total_reward, i
 
-
-def propose_policy(current_policy):
-    print(current_policy[1, 1, 1])
-    return current_policy
+    return total_reward, step_count
 
 
 if __name__ == '__main__':
-    n_iter = 100000
-    n_samples = 10
+    n_iter = 10000
     logdir = 'logdir'
 
-    gym.envs.registration.register(
-        id='openmaze-custom-v0',
-        entry_point='gym_openmaze.envs.openmaze:OpenMaze',
-        kwargs={'size': (10, 7), 'random': False},
-        max_episode_steps=100000
-    )
-    env = gym.make('openmaze-custom-v0')  # TODO: change to random
-    policy_shape = [*env.observation_space.shape, env.action_space.n]
+    env = gym.make('openmaze-v0')
 
-    with tf.name_scope("model"):
-        bias = np.array([.575, .2, .025, .2], dtype=np.float32)
-        policy = ed.models.Multinomial(total_count=1., probs=np.broadcast_to(bias, policy_shape))
-        # policy = ed.models.DirichletMultinomial(total_count=1., concentration=tf.ones(policy_shape))
-        proposal_policy = ed.models.Normal(loc=policy, scale=10., value=policy)
+    actions = {0: 'N', 1: 'E', 2: 'S', 3: 'W'}
+    bias = np.array([.575, .2, .025, .2], dtype=np.float32)  # TODO: learn this
+    policy_shape = [*env.observation_space.shape, 1]
+    policy_len = np.multiply(*env.observation_space.shape)
 
-        policy_value = tf.placeholder(tf.float32)
-        r = ed.models.Binomial(total_count=1., probs=policy_value, value=1.)
-        # policy_value = ed.models.Normal(loc=.5, scale=0.5)
-        # policy_value_proposal = ed.models.Normal(loc=policy_value, scale=0.5)
-        # policy_value_proposal = tf.placeholder(tf.float32)
+    # define the model
+    policy = {}
+    latent_vars = {}
+    proposal_vars = {}
+    it = 0
+    for y in range(policy_shape[0]):
+        for x in range(policy_shape[1]):
+            avail_actions = env.action_space.available_actions(y, x)
+            if not avail_actions:
+                continue
 
-    with tf.name_scope("posterior"):
-        q_policy = ed.models.Empirical(params=tf.Variable(tf.zeros([n_iter, *policy_shape])))
+            b = _normalize(np.array([bias[a] if a in avail_actions else 0. for a in actions.keys()], dtype=np.float32))
+            pi = ed.models.Multinomial(total_count=1., probs=b)
+            pi_posterier = ed.models.Empirical(params=tf.Variable(tf.zeros([n_iter, *b.shape])))
+            pi_proposal = ed.models.Normal(loc=pi, scale=10., value=pi)
 
-    inference = ed.MetropolisHastings(latent_vars={policy: q_policy},
-                                      proposal_vars={policy: proposal_policy},
-                                      data={r: 1.0})
+            policy[(y, x)] = pi
+            latent_vars[pi] = pi_posterier
+            proposal_vars[pi] = pi_proposal
+            it += 1
+    policy_value = tf.placeholder(tf.float32)
+    r = ed.models.Binomial(total_count=1., probs=policy_value, value=1.)
 
+    inference = ed.MetropolisHastings(latent_vars=latent_vars, proposal_vars=proposal_vars, data={r: 1.0})
     inference.initialize(logdir=logdir)
     tf.global_variables_initializer().run()
 
+    sess = ed.get_session()
     with tqdm.trange(inference.n_iter, desc='inference') as progress_bar:
         for _ in progress_bar:
-            v, i = run_maze(env, ed.get_session().run(policy))
-            info = inference.update(feed_dict={policy_value: v})
-            progress_bar.set_postfix({'accept_rate': info['accept_rate'], 'val': v, 'maze_it': i})
-    run_maze(env, ed.get_session().run(policy), render_mode='human')
+            value, i = run_maze(env, {key: sess.run(val) for key, val in policy.items()})
+            info = inference.update(feed_dict={policy_value: value})
+            progress_bar.set_postfix(accept_rate=info['accept_rate'], val=value, maze_it=i)
+        final_policy = {key: actions[int(np.argmax(sess.run(val)))] for key, val in policy.items()}
+        progress_bar.write(pprint.pformat(final_policy))
     inference.finalize()
