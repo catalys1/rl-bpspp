cd40a26b5c08161488c4f10758c33889b070968a
William Myers <mwilliammyers@gmail.com>
2018-03-08 17:33:13 -0700

    Update main.py
    - WIP

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import random
+
 import gym
 import gym_openmaze
 import numpy as np
-from tqdm import trange
 import scipy.stats
+from tqdm import trange
 
 
 class Model:
@@ -18,13 +20,22 @@ class Model:
 
         self._policy = scipy.stats.multinomial(n=1, p=self.bias)
         self.policy = self._policy.rvs(size=env.observation_space.shape)
+        # for p in self.policy:
+        #     print(p.shape)
+        #     for i in p:
+        #         print(i.shape)
+        # self.reward = 0.0
+        # self.log_likelihood = 0.0
+        # self.path = [tuple(idx) for idx in np.argwhere(self.policy[:, :, 0] == 1)]
+        self.path, self.reward = self.run()
+        self.log_likelihood = self.compute_log_likelihood()
 
-        self.reward = 0.0
-        self.log_likelihood = np.log(self.reward)
-
-    def propose_policy(self, from_policy=None, num_states_to_change=1):
+    def propose_policy(self, from_policy=None, from_path=None, num_states_to_change=1):
         if from_policy is None:
             from_policy = self.policy
+
+        if from_path is None:
+            from_path = self.path
 
         if self._bias is not None and np.random.randint(0, 2) == 0:
             # TODO: this doesnt seem right? just sample it again?
@@ -33,12 +44,16 @@ class Model:
         else:
             from_policy = np.copy(from_policy)
             for i in range(num_states_to_change):
-                state_row = np.random.randint(0, from_policy.shape[0])
-                state_col = np.random.randint(0, from_policy.shape[1])
+                print(from_path)
                 action = np.random.randint(0, self.env.action_space.n)
+                # action = np.random.choice(self.env.action_space.n, p=self.bias)
                 distribution = np.zeros(self.env.action_space.n)
                 distribution[action] = 1.0
-                from_policy[state_row, state_col] = distribution
+                state = random.choice(list(from_path))
+                # print(state)
+                # print(from_policy[state])
+                from_policy[state] = distribution
+                # print(from_policy[state])
 
         return from_policy
 
@@ -46,10 +61,12 @@ class Model:
         if policy is None:
             policy = self.policy
 
-        state = self.env.reset()
         total_reward = 0.0
+
+        state = tuple(self.env.reset())
+        path = {state}
         while True:
-            action = np.random.choice(self.env.action_space.n, p=policy[tuple(state)])
+            action = np.random.choice(self.env.action_space.n, p=policy[state])
             observation, reward, done, info = self.env.step(action)
 
             if render_mode is not None:
@@ -59,9 +76,9 @@ class Model:
 
             if done:
                 break
-            state = observation
-
-        return total_reward
+            state = tuple(observation)
+            path.add(state)
+        return path, total_reward
 
     def compute_log_likelihood(self, policy=None, reward=None):
         if policy is None:
@@ -78,10 +95,10 @@ class Model:
 def metropolis_hastings(model, iterations=1000):
     acceptance_count = 0
     with trange(iterations) as progress:
-        progress.set_description('a: inf, acceptance count: 0')
+        progress.set_description('a: 0.0, acceptance count: 0')
         for _ in progress:
             policy_prime = model.propose_policy()
-            reward_prime = model.run(policy=policy_prime)
+            path_prime, reward_prime = model.run(policy=policy_prime)
 
             log_likelihood_prime = model.compute_log_likelihood(policy=policy_prime, reward=reward_prime)
             a = log_likelihood_prime - model.log_likelihood
@@ -89,6 +106,7 @@ def metropolis_hastings(model, iterations=1000):
                 model.policy = policy_prime
                 model.reward = reward_prime
                 model.log_likelihood = log_likelihood_prime
+                model.path = path_prime
                 acceptance_count += 1
                 progress.set_description('a: {}, acceptance count: {}'.format(a, acceptance_count))
     return model
@@ -102,6 +120,14 @@ def main(search_for_policy=metropolis_hastings):
         max_episode_steps=10000
     )
     env = gym.make('openmaze-custom-v0')  # TODO: change to random
+
+    # gym.envs.registration.register(
+    #     id='NChain-custom-v0',
+    #     entry_point='gym.envs.toy_text:NChainEnv',
+    #     kwargs={'n': 5, 'slip': 0.0},
+    #     max_episode_steps=1000
+    # )
+    # env = gym.make('NChain-custom-v0')
 
     # model = Model(env, bias=[.85, .05, .05, .05])
     model = Model(env, bias=None)
