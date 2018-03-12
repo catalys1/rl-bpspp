006c0af99a7beab7a5f7903037ea67cc24bee619
William Myers <mwilliammyers@gmail.com>
2018-03-12 01:16:32 -0600

    Add hack to run final policy

--- maze_probpy.py
+++ maze_probpy.py
@@ -25,7 +25,7 @@ def explore(env, policy, render_mode=None):
 
         if render_mode is not None:
             env.render(mode=render_mode)
-            time.sleep(.5)
+            time.sleep(.1)
 
         total_reward += reward
 
@@ -44,16 +44,16 @@ def model(pp, env, actions, render_mode=None):
         pp.random(name='bias', loop_iter=i) for i in range(env.action_space.n)
     ])
 
-    policy = np.zeros(
-        (*env.observation_space.shape, env.action_space.n), dtype=np.int8)
+    policy_shape = (*env.observation_space.shape, env.action_space.n)
+    policy = np.zeros(policy_shape, dtype=np.int8)
     i = 0
     for y in range(policy.shape[0]):
         for x in range(policy.shape[1]):
             available_actions = env.action_space.available_actions(y, x)
             if np.any(available_actions):
                 bias = _normalize(np.multiply(global_bias, available_actions))
-                action_idx = pp.choice(
-                    elements=actions, p=bias, name='policy', loop_iter=i)
+                name = 'policy_{}_{}_'.format(y, x)
+                action_idx = pp.choice(name, actions, p=bias, loop_iter=i)
                 # one-hot, eg: [0, 1, 0, 0] == 'E'
                 policy[y, x, action_idx] = 1
             i += 1
@@ -71,8 +71,8 @@ if __name__ == '__main__':
     np.seterr(all='warn')
 
     # env_id = 'OpenMazeUrgencyMax10CyclesInWindowSize2-v0'
-    env_id = 'OpenMazeUrgencyMax20CyclesInWindowSize2-v0'
-    # env_id = 'OpenMazeOnlyCompletionRewardMax20CyclesInWindowSize2-v0'
+    # env_id = 'OpenMazeUrgencyMax4CyclesInWindowSize2-v0'
+    env_id = 'OpenMazeOnlyCompletionRewardMax4CyclesInWindowSize2-v0'
     env = gym.make(env_id)
 
     try:
@@ -89,23 +89,30 @@ if __name__ == '__main__':
     driver = InferenceDriver(lambda pp: model(pp, env, actions))
     driver.condition(label='r-0', value=1)
     driver.init_model()
-    driver.prior(label="bias-0", value=.575)
-    driver.prior(label="bias-1", value=.2)
-    driver.prior(label="bias-2", value=.025)
-    driver.prior(label="bias-3", value=.2)
+    # driver.prior(label="bias-0", value=.575)
+    # driver.prior(label="bias-1", value=.2)
+    # driver.prior(label="bias-2", value=.025)
+    # driver.prior(label="bias-3", value=.2)
 
-    driver.burn_in(steps=500)
+    driver.burn_in(steps=100)
 
-    for k, v in driver.run_inference(interval=5, samples=5000).items():
-        print(k, v)
+    policy_shape = (*env.observation_space.shape, env.action_space.n)
+    posterior_policy = np.zeros(policy_shape, dtype=np.int8)
+    for k, v in driver.run_inference(interval=1, samples=500).items():
+        print(k, {
+            'value': v['value'],
+            'probs': v['parameters'].get('p'),
+            'likelihood': v['likelihood']
+        })
+        if 'policy' in k:
+            _, y, x, _ = k.split('_')
+            posterior_policy[int(y), int(x), int(v['value'])] = 1
 
-    driver.plot_ll()
+    # driver.plot_ll()
 
-    rewards = driver.plot_model_results()
-    print('len(rewards):', len(rewards))
-    # print(rewards)
-    print('avg:', np.mean(rewards))
-    print('completion count:',
-          np.count_nonzero(np.isclose(rewards, env.reward_range[1])))
+    # rewards = driver.plot_model_results()
+    # print('avg:', np.mean(rewards))
+    # print('completion count:',
+    #       np.count_nonzero(np.isclose(rewards, env.reward_range[1])))
 
-    driver.finalize(lambda pp: model(pp, env, actions, render_mode='human'))
+    explore(env, posterior_policy, render_mode='human')
